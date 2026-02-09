a86b1d3fc02817d365da7ff81b0a54a5656cb80d
Knights879 <42558531+Knights879@users.noreply.github.com>
2026-02-09 16:00:14 -0500

    fixed typo in C header file

--- C/basics.h
+++ C/basics.h
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-// Use a header file for managing a includes (called imports in other languages),
+// Use a header file for managing and including (called imports in other languages),
 // things like `typedefs`, constants, and global variables, and functional prototypes.
 
 
